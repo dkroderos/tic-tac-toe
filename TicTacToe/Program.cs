9d8e16ab60a9852f540c9c796519f880812b1685
DavidKingRoderos <75028710+DavidKingRoderos@users.noreply.github.com>
2021-01-20 13:03:29 +0800

    Udpdate

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,12 +6,13 @@ namespace TicTacToe
 {
     class Program
     {
-
         private static int[] player_O = new int[9]; // Coordinates occupied by the player O
 
         private static int[] player_X = new int[9]; // Coordinates occupied by the player X
         
         private static int input;
+
+        private static  Dictionary<int, char> spaceLocations;
 
         // Tic Tac Toe Drawing
         private static char[,] ticTacToe = {
@@ -24,24 +25,69 @@ namespace TicTacToe
 
         static void Main(string[] args)
         {
+            spaceLocations = CreateSpaceLocations();
+
             for (int turn = 0; turn < 9; turn++) 
             {
                 if (turn % 2 == 1) // Making sure that players are switching turns 
                 {
+                    PrintTicTacToe(ticTacToe);
                     Console.Write("Circle's Turn: ");
-                    input = int.Parse(Console.ReadLine());
+                    PlayersTurn(player_O, input, spaceLocations, 'O');
+                    if (PlayerHasWon(player_O))
+                    {
+                        Console.WriteLine("X has won.");
+                        return;
+                    }
                 }
                 else
                 {
+                    PrintTicTacToe(ticTacToe);
                     Console.Write("X's Turn: ");
-                    input = int.Parse(Console.ReadLine());
+                    PlayersTurn(player_X, input, spaceLocations, 'X');
+                    if (PlayerHasWon(player_X))
+                    {
+                        Console.WriteLine("X has won.");
+                        return;
+                    }
                 }
             }
         }
 
-        static void PlayersTurn(int[] player, int number)
+        static void PlayersTurn(int[] player, int input, Dictionary<int, char> spaceLocations, char symbol) // Player is making a turn
         {
+            input = int.Parse(Console.ReadLine());
+            if (input < 1 || input > 9)
+            {
+                Console.Write("Please enter a number in range 1 - 9: ");
+                PlayersTurn(player, input, spaceLocations, symbol);
+            }
+            if (spaceLocations[input] != ' ')
+            {
+                Console.Write("The coordinate is already occupied: ");
+                PlayersTurn(player, input, spaceLocations, symbol);
+            }
+            else
+            {
+                player[input - 1] = input;
+                spaceLocations[input] = symbol;
+            }
+        }
 
+        static Dictionary<int, char> CreateSpaceLocations() // Make a dictionary to easily change the Tic Tac Toe board
+        {
+            Dictionary<int, char> spaceLocations = new Dictionary<int, char>();
+            spaceLocations[1] = ticTacToe[0, 0];
+            spaceLocations[2] = ticTacToe[0, 2];
+            spaceLocations[3] = ticTacToe[0, 4];
+            spaceLocations[4] = ticTacToe[2, 0];
+            spaceLocations[5] = ticTacToe[2, 2];
+            spaceLocations[6] = ticTacToe[2, 4];
+            spaceLocations[7] = ticTacToe[4, 0];
+            spaceLocations[8] = ticTacToe[4, 2];
+            spaceLocations[9] = ticTacToe[4, 4];
+
+            return spaceLocations;
         }
 
         private static void PrintTicTacToe(char[,] ticTacToe)
@@ -53,8 +99,8 @@ namespace TicTacToe
                 {
                     Console.Write(ticTacToe[i, j]);
                 }
+                Console.WriteLine();
             }
-            Console.WriteLine();
         }
 
         private static bool IsCoordinatesOccupiedByPlayer(int[] playerCoords, params int[] sequence)
