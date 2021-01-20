2b6b619491b4ff5a0616b312f4f20fd33dcb8a8b
DavidKingRoderos <75028710+DavidKingRoderos@users.noreply.github.com>
2021-01-20 11:04:25 +0800

    Quick update

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,9 +4,34 @@ namespace TicTacToe
 {
     class Program
     {
+
+        private static int[] playerCircle = new int[9];
+
+        private static int[] playerX = new int[9];
+
+        private static char[,] ticTacToe = 
+        {
+            { ' ', '|', ' ', '|', ' ' },
+            { '-', '-', '-', '-', '-' },
+            { ' ', '|', ' ', '|', ' ' },
+            { '-', '-', '-', '-', '-' },
+            { ' ', '|', ' ', '|', ' ' }
+        };
+
         static void Main(string[] args)
         {
             
+        }
+
+        private static void PrintTicTacToe(char[,] ticTacToe)
+        {
+            for (int i = 0; i < ticTacToe.GetLength(0); i++)
+            {
+                for (int j = 0; j < ticTacToe.GetLength(1); j++)
+                {
+                    Console.Write(ticTacToe[i, j]);
+                }
+            }
         }
     }
 }
