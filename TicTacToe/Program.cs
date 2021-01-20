342c25ba56e89976f6960b7a9a81f8853370705b
DavidKingRoderos <75028710+DavidKingRoderos@users.noreply.github.com>
2021-01-20 11:47:02 +0800

    Making methods Update

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -22,7 +22,13 @@ namespace TicTacToe
         static void Main(string[] args)
         {
             int turn = 0;
-
+            for (turn; turn < 9; turn++) 
+            {
+                if (turn % 2 == 1) // Making sure that players are switching turns 
+                {
+                    
+                }
+            }
         }
 
         private static void PrintTicTacToe(char[,] ticTacToe)
@@ -50,6 +56,57 @@ namespace TicTacToe
             }
 
             return currentIndex == sequence.Length;
+        }
+
+        private static bool PlayerHasWon(int[] player)
+        {
+            // Checks if a player as won
+            if (HasWonHorizontally(player) ||
+                HasWonVertically(player) ||
+                HasWonDiagonally(player))
+            {
+                return true;
+            }
+
+            else return false;
+        }
+
+        private static bool HasWonHorizontally(int[] player)
+        {
+            // Checks if a player won horizontally
+            if (IsCoordinatesOccupiedByPlayer(player, 1, 2, 3) || 
+                IsCoordinatesOccupiedByPlayer(player, 4, 5, 6) ||
+                IsCoordinatesOccupiedByPlayer(player, 7, 8, 9))
+            {
+                return true;
+            }
+
+            else return false;
+        }
+
+        private static bool HasWonVertically(int[] player)
+        {
+            // Checks if a player won vertically
+            if (IsCoordinatesOccupiedByPlayer(player, 1, 4, 7) ||
+                IsCoordinatesOccupiedByPlayer(player, 2, 5, 8) ||
+                IsCoordinatesOccupiedByPlayer(player, 3, 6, 9))
+            {
+                return true;
+            }
+
+            else return false;
+        }
+
+        private static bool HasWonDiagonally(int[] player)
+        {
+            // Checks if a player won diagonally
+            if (IsCoordinatesOccupiedByPlayer(player, 1, 5, 9) ||
+                IsCoordinatesOccupiedByPlayer(player, 3, 5, 7))
+            {
+                return true;
+            }
+
+            else return false;
         }
     }
 }
