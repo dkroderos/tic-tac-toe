829f871fdd604d7109311ebdbe3e5d93349bfd29
DavidKingRoderos <75028710+DavidKingRoderos@users.noreply.github.com>
2021-01-20 11:25:04 +0800

    Quick update

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -5,26 +5,29 @@ namespace TicTacToe
     class Program
     {
 
-        private static int[] playerCircle = new int[9];
+        private static int[] player_O = new int[9]; // Coordinates occupied by the player O
 
-        private static int[] playerX = new int[9];
+        private static int[] player_X = new int[9]; // Coordinates occupied by the player X
 
-        private static char[,] ticTacToe = 
-        {
+
+        // Tic Tac Toe Drawing
+        private static char[,] ticTacToe = {
             { ' ', '|', ' ', '|', ' ' },
             { '-', '-', '-', '-', '-' },
             { ' ', '|', ' ', '|', ' ' },
             { '-', '-', '-', '-', '-' },
             { ' ', '|', ' ', '|', ' ' }
-        };
+        }; 
 
         static void Main(string[] args)
         {
-            
+            int turn = 0;
+
         }
 
         private static void PrintTicTacToe(char[,] ticTacToe)
         {
+            // Prints the Tic Tac Toe
             for (int i = 0; i < ticTacToe.GetLength(0); i++)
             {
                 for (int j = 0; j < ticTacToe.GetLength(1); j++)
@@ -32,6 +35,21 @@ namespace TicTacToe
                     Console.Write(ticTacToe[i, j]);
                 }
             }
+            Console.WriteLine();
+        }
+
+        private static bool IsCoordinatesOccupiedByPlayer(int[] playerCoords, params int[] sequence)
+        {
+            // Checks if a player has created a line
+            int currentIndex = 0;
+
+            foreach (int coordinate in playerCoords)
+            {
+                if (currentIndex == sequence.Length) return true;
+                if (sequence[currentIndex] == coordinate) currentIndex++;
+            }
+
+            return currentIndex == sequence.Length;
         }
     }
 }
